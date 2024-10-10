9f4bc38166234293e627294b9ed2b84d675e3d7c
nebilesamelsuleymna <nebilesmaelprog@gmail.com>
2024-10-10 03:26:58 -0700

    adding review cards temp

--- Routers/viewroutes.js
+++ Routers/viewroutes.js
@@ -3,13 +3,8 @@ const Router =express.Router();
 const viewsController =require('./../controllers/viewController');
 const { getOverview } = require('../controllers/viewController');
 
-Router.get('/',(req,res)=>{
-    res.status(200).render('base',{
-    tour:'the first hiker',
-    user:'nebiloo'
-    })
-})
-Router.get('/overview',getOverview);
+
+Router.get('/',getOverview);
 Router.get('/tour/:slug',viewsController.getTour)
 
 module.exports=Router
